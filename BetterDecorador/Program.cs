ab629ed6029497ea58d99704bf6d23838741a181
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-03-13 22:59:35 +0000

    setup

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Decor("Good Morning!", '*', 2));
+            Console.WriteLine(Decor("Good Morning!", '.', 2));
+        }
+        private static string Decor(string s, char dec, int i)
+        {
+
+            return $"{dec*i} {s} {dec+i}";
+        }
+    }
+}
